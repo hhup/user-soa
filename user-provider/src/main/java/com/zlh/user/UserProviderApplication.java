f324394317a11a837697b0c36ed16e6816030129
zhaoliheng <hhup2000@163.com>
2019-04-23 11:14:38 +0800

    项目初建

--- user-provider/src/main/java/com/zlh/user/UserProviderApplication.java
+++ user-provider/src/main/java/com/zlh/user/UserProviderApplication.java
@@ -1,8 +1,12 @@
-package com.zlh.userprovider;
+package com.zlh.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserProviderApplication {
 
